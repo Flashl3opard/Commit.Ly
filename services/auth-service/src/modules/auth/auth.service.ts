c81457daf5615dd82b153f46d8bf9622ee8a8912
FlashL3opard <69573060+Flashl3opard@users.noreply.github.com>
2026-08-31 03:41:57 +0530

    Move auth app into services/auth-service

--- services/auth-service/src/modules/auth/auth.service.ts
+++ services/auth-service/src/modules/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { prisma } from "../../config/prisma";
+import { prisma } from "@commitly/db";
 import { signToken } from "../../utils/jwt";
 import type { RegisterInput, LoginInput } from "./auth.validation";
 
